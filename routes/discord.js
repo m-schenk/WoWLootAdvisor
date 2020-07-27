3a15f6bf3ae5c52a2f98de72387b8740edd5ccda
sandro <sandro.montanaro@gmail.com>
2020-07-28 01:02:09 +0200

    added discord authentication, user object can be retrieved!

--- routes/discord.js
+++ routes/discord.js
@@ -6,7 +6,7 @@ const DiscordOauth2 = require("discord-oauth2");
 const oauth = new DiscordOauth2();
 const fetch = require('node-fetch');
 const btoa = require('btoa');
-const redirect = encodeURIComponent('http://localhost:50451/api/discord/success');
+const redirect = 'http://localhost:3000/discord/success';
 const { catchAsync } = require('../utils');
 
 // oauth.tokenRequest({
@@ -29,8 +29,7 @@ const { catchAsync } = require('../utils');
 
 
 router.get('/login', (req, res) => {
-  const url = 'https://discord.com/api/oauth2/authorize?client_id=734533006114553866&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord%2Fsuccess&response_type=code&scope=identify';
-  
+  const url = 'https://discord.com/api/oauth2/authorize?client_id=734533006114553866&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord%2Fsuccess&response_type=code&scope=identify%20guilds%20email';
   res.redirect(url);
 });
 
@@ -41,15 +40,38 @@ router.get('/success', catchAsync(async (req, res) => {
   
   
   const creds = btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`);
-  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
-    {
+  const data = {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    grant_type: 'authorization_code',
+    redirect_uri: redirect,
+    code: code,
+    scope: 'identify guilds email'
+  };
+  const response = await fetch('https://discordapp.com/api/oauth2/token', {
       method: 'POST',
+      body: new URLSearchParams(data),
       headers: {
-        Authorization: `Basic ${creds}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
     });
   const json = await response.json();
   console.log('fuckyou', json);
+
+  console.log('ACCESS TOKEN:', json.access_token);
+  
+
+  const gotodiscordplease = await fetch('http://discordapp.com/api/users/@me', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${json.access_token}`,
+    },
+
+  });
+  const user = await gotodiscordplease.json();
+  
+  console.log('user object:', user);
+  
   
   res.redirect(`/?token=${json.access_token}`);
 }));
