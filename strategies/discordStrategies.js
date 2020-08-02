ec64c73642c6a163d524f5965f2f4399c0ebbf97
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-02 23:32:36 +0200

    Update discordStrategies.js

--- strategies/discordStrategies.js
+++ strategies/discordStrategies.js
@@ -2,30 +2,40 @@ const DiscordStrategy = require('passport-discord').Strategy;
 const passport = require('passport');
 const Player = require('../models/Player');
 
+passport.serializeUser((player, done) => {
+    done(null, player._id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(_id, (err, player) => {
+        done(err, player);
+    });
+});
+
 passport.use(new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.CLIENT_REDIRECT,
     scope: ['identify', 'guilds']
 }, (accessToken, refreshToken, profile, cb) => {
-    Player.findOne({discordId: profile.id}).
-    then(player => {
-        if(player) {
-            cb(null, player);
-        } else {
-            console.log(profile.guilds.filter(entry => (entry.id === process.env.DISCORD_SERVER_ID)))
-            if(profile.guilds.filter(entry => (entry.id === process.env.DISCORD_SERVER_ID))) {
-                const newPlayer = new Player({
-                    discordId: profile.id
-                })
-                newPlayer.save();
-                cb(null, newPlayer)
+    Player.findOne({ discordId: profile.id }).
+        then(player => {
+            if (player) {
+                cb(null, player);
             } else {
-                cb(new Error('Access denied, does not belong to guild!'), null);
+                console.log(profile.guilds.filter(entry => (entry.id === process.env.DISCORD_SERVER_ID)))
+                if (profile.guilds.filter(entry => (entry.id === process.env.DISCORD_SERVER_ID))) {
+                    const newPlayer = new Player({
+                        discordId: profile.id
+                    })
+                    newPlayer.save();
+                    cb(null, newPlayer)
+                } else {
+                    cb(new Error('Access denied, does not belong to guild!'), null);
+                }
             }
-        }
-    })
-    .catch(err => {
-        cb(err, null)
-    })
+        })
+        .catch(err => {
+            cb(err, null)
+        })
 }));
