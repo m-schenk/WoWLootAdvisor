7f36c59388eca64e0f7ecbbafaa996d335650fc6
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-02 12:08:16 +0200

    test aws

--- controllers/items.js
+++ controllers/items.js
@@ -1,119 +1,96 @@
-const Item = require('../models/Item');
-
-exports.getMockData = (req, res, next) => {
-    const item = new Item({
-
-        id: 12345,
-        name: 'Blade of Abortion',
-        itemType: 'Pole',
-        itemCategory: 'Limited',
-        raid: 'AQ40',
-        encounters: ['Boss1', 'Trash1', 'Trash2'],
-        priority: [
-            {classes: ['Mage', 'Warlock']},
-            {classes: ['Warrior', 'Paladin']}
-        ],
-
-    })
-
-    item.save()
-    .then(item => {
-        console.log('Object saved');
-        res.status(200).json({message: 'success', item: item});
-    })
-    .catch(err => {
-        const error = new Error(err);
-        error.message = 'Failed to save item (controllers/items getMockData())'
-        error.httpStatusCode = 500;
-        return next(error);
-      });
-}
+const Item = require("../models/Item");
 
 exports.modify = (req, res, next) => {
-
     const item = req.body;
 
     console.log(item);
-    
+
     if (item.itemCategory) {
-        Item.update({id: req.body.id}, {
-            $set: {
-                itemCategory: req.body.itemCategory
+        Item.update(
+            { id: req.body.id },
+            {
+                $set: {
+                    itemCategory: req.body.itemCategory,
+                },
             }
-        })
-        .then(item => {
-            res.status(201).json({
-                message: 'new category has been set',
-                value: req.body.itemCategory
+        )
+            .then((item) => {
+                res.status(201).json({
+                    message: "new category has been set",
+                    value: req.body.itemCategory,
+                });
+            })
+            .catch((err) => {
+                const error = new Error(err);
+                error.message = "Failed to update item (controllers/items modify())";
+                error.httpStatusCode = 500;
+                return next(error);
             });
-        })
-        .catch(err => {
-          const error = new Error(err);
-          error.message = 'Failed to update item (controllers/items modify())'
-          error.httpStatusCode = 500;
-          return next(error);
-        });
     }
 
     if (item.priority) {
-        Item.update({id: req.body.id}, {
-            $set: {
-                priority: req.body.priority
+        Item.update(
+            { id: req.body.id },
+            {
+                $set: {
+                    priority: req.body.priority,
+                },
             }
-        })
-        .then(item => {
-            res.status(201).json({
-                message: 'new priority has been set',
-                value: req.body.itemCategory
+        )
+            .then((item) => {
+                res.status(201).json({
+                    message: "new priority has been set",
+                    value: req.body.itemCategory,
+                });
+            })
+            .catch((err) => {
+                const error = new Error(err);
+                error.message = "Failed to update item (controllers/items modify())";
+                error.httpStatusCode = 500;
+                return next(error);
             });
-        })
-        .catch(err => {
-          const error = new Error(err);
-          error.message = 'Failed to update item (controllers/items modify())'
-          error.httpStatusCode = 500;
-          return next(error);
-        });
     }
-}
+};
 
 exports.getQuery = (req, res, next) => {
-
     let query = req.query.query;
     let regex = new RegExp(query, "i");
-    
-    Item.find({name: regex})
-    .sort({ name: 1})
-    .limit(15)
-    .or([{ itemCategory: 'Reserved' }, { itemCategory: 'Limited' }, { itemCategory: 'Unlimited' }]) //dumb way to filter "Unlockable" itemCategory but couldn't find a "not" function
-    .then(item => {
-        res.status(200).json({
-            results: item,
-            statusText: "OK"
+
+    Item.find({ name: regex })
+        .sort({ name: 1 })
+        .limit(15)
+        .or([
+            { itemCategory: "Reserved" },
+            { itemCategory: "Limited" },
+            { itemCategory: "Unlimited" },
+        ]) //dumb way to filter "Unlockable" itemCategory but couldn't find a "not" function
+        .then((item) => {
+            res.status(200).json({
+                results: item,
+                statusText: "OK",
+            });
         })
-    })
-    .catch(err => {
-        const error = new Error(err);
-        error.message = 'Failed to fetch item from database (controllers/items getQuery())';
-        error.httpStatusCode = 500;
-        return next(error);
-    });
-}
+        .catch((err) => {
+            const error = new Error(err);
+            error.message =
+                "Failed to fetch item from database (controllers/items getQuery())";
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
 
 exports.getItemById = (req, res, next) => {
-
     const itemId = req.params.id;
 
     Item.findOne({ id: itemId })
-    .then(item => {
-        //console.log(item);
-        
-        res.status(200).json({ item: item });
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.message = 'Failed to fetch player from database (controllers/items getItemById())';
-      error.httpStatusCode = 500;
-      return next(error);
-  });
-}
-
+        .then((item) => {
+            res.status(200).json({ item: item });
+        })
+        .catch((err) => {
+            const error = new Error(err);
+            error.message =
+                "Failed to fetch player from database (controllers/items getItemById())";
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
--- controllers/player.js
+++ controllers/player.js
@@ -1,8 +1,7 @@
-const Player = require('../models/Player');
-const Wishlist = require('../models/Wishlist');
+const Player = require("../models/Player");
+const Wishlist = require("../models/Wishlist");
 
 exports.addPlayer = (req, res, next) => {
-    
     const player = new Player({
         id: req.body.id,
         name: req.body.name,
@@ -11,64 +10,65 @@ exports.addPlayer = (req, res, next) => {
         talent: req.body.talent,
         aq_attendance: req.body.aq_attendance,
         naxx_attendance: req.body.naxx_attendance,
-        wishlist: req.body.wishlist
+        wishlist: req.body.wishlist,
     });
 
-    player.save()
-    .then(player => {
-        res.status(201).json({ message: 'Player created successfully'});
-    })
-    .catch(err => {
-        const error = new Error(err);
-        error.message = 'Failed to save new player in database (controllers/player addPlayer())';
-        error.httpStatusCode = 500;
-        return next(error);
-    });
-}
-
+    player
+        .save()
+        .then((player) => {
+            res.status(201).json({ message: "Player created successfully" });
+        })
+        .catch((err) => {
+            const error = new Error(err);
+            error.message =
+                "Failed to save new player in database (controllers/player addPlayer())";
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
 
 exports.getPlayerById = (req, res, next) => {
     const playerId = req.params.id;
 
-    console.log('id', playerId);
-    
+    console.log("id", playerId);
 
     Player.findOne({ id: playerId })
-    .populate({ path: 'wishlist', model: Wishlist})
-    .then(player => {
-        console.log(player);
-        
-        res.status(200).json({ player: player });
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.message = 'Failed to fetch player from database (controllers/player getPlayerById())';
-      error.httpStatusCode = 500;
-      return next(error);
-    });
-}
+        .populate({ path: "wishlist", model: Wishlist })
+        .then((player) => {
+            console.log(player);
+
+            res.status(200).json({ player: player });
+        })
+        .catch((err) => {
+            const error = new Error(err);
+            error.message =
+                "Failed to fetch player from database (controllers/player getPlayerById())";
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
 
 exports.getSessionPlayerId = (req, res, next) => {
-  res.status(200).json({ playerId: req.session.playerId })
-}
+    res.status(200).json({ playerId: req.session.playerId });
+};
 
 exports.playerLogout = (req, res, next) => {
     req.session.destroy((err) => {
-        console.log('logged out');
+        console.log("logged out");
         res.redirect(process.env.DISCORD_CALLBACK_URI);
-    })
-}
+    });
+};
 
 exports.getPlayerSelf = (req, res, next) => {
-  Player.find({ id: req.session.playerId })
-  .then(player => {
-    res.send(200).json({ player: player })
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.message = 'Failed to fetch player from database (controllers/player getPlayerSelf())';
-    error.httpStatusCode = 500;
-    return next(error);
-  });
-  
-}
+    Player.find({ id: req.session.playerId })
+        .then((player) => {
+            res.send(200).json({ player: player });
+        })
+        .catch((err) => {
+            const error = new Error(err);
+            error.message =
+                "Failed to fetch player from database (controllers/player getPlayerSelf())";
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
