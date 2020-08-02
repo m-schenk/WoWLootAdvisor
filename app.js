9ce5bc65c032f0da0b1bf8849a41d761fb6a4441
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-03 01:48:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,11 @@ client.on('messageReactionAdd', async (reaction, user) => {
 	// Now the message has been cached and is fully available
 	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
 	// The reaction is now also fully available and the properties will be reflected accurately:
-	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+    
+    if(reaction.emoji.name === ":handshake:") {
+        console.log(reaction.users);
+    }
 });
 
 // view engine setup
