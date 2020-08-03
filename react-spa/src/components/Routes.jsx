9beecd810e8bf5bc3bb38bb1badc50027835e8aa
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-04 01:25:01 +0200

    Update Routes.jsx

--- react-spa/src/components/Routes.jsx
+++ react-spa/src/components/Routes.jsx
@@ -20,6 +20,11 @@ import './assets/App.css';
 
 
 class Routes extends React.Component {
+
+    componentWillUpdate() {
+        this.props.overmind.actions.isAuthenticated()
+    }
+
     render() {
         return(
             <Router>
@@ -27,7 +32,7 @@ class Routes extends React.Component {
                         <Route path="/login" component={Login} />
                         <div className="justify-content-center align-items-center" id="wrapper">
                             <div className="justify-content-center align-items-center" id="header"><Navbar /></div>
-                                <AuthContext.Provider value={() => { this.props.overmind.actions.isAuthenticated() }}>
+                                <AuthContext.Provider value={this.props.overmind.state.isAuth}>
                                     <ProtectedRoute exact path="/" component={MainContent} />
                                     <ProtectedRoute path="/wishlist" component={MainContent} />
                                     <ProtectedRoute path="/profile" component={Profile} />
