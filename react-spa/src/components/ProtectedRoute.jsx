fd074334ac1da02e5e5e364f11130151bc9ca95a
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-03 23:59:38 +0200

    Update ProtectedRoute.jsx

--- react-spa/src/components/ProtectedRoute.jsx
+++ react-spa/src/components/ProtectedRoute.jsx
@@ -5,12 +5,13 @@ import { connect } from '../overmind';
 class ProtectedRoute extends React.Component {
     render() {
         const isAuthenticated = this.props.overmind.actions.isAuthenticated()
+        const Component = this.props.component;
         return (
             <Route
                 render={
                     props => {
                         return isAuthenticated ?
-                            <this.props.Component {...props} /> :
+                            <Component {...props} /> :
                             <Redirect to='/login' />
                     }}
             />
