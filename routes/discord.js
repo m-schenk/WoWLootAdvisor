da2ff15968b509c701743f91ec4b75072f11635f
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-03 17:57:43 +0200

    Update discord.js

--- routes/discord.js
+++ routes/discord.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 
 router.get('/login', passport.authenticate('discord'));
 router.get('/redirect', passport.authenticate('discord', {
-    failureRedirect: '/forbidden',
-    successRedirect: '../../profile'
+    failureRedirect: 'http://raegae.maarten.ch:3000/forbidden',
+    successRedirect: 'http://raegae.maarten.ch:3000/profile'
 }), (req, res) => {
     res.send(req.user);
 });
