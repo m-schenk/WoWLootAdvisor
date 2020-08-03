df75d9e1795d87b7443d75e9ed366a8844df6738
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-04 01:39:30 +0200

    Update ProtectedRoute.jsx

--- react-spa/src/components/ProtectedRoute.jsx
+++ react-spa/src/components/ProtectedRoute.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+    console.log(isAuthenticated)
     return (
         <Route 
             {...rest} 
             render={
             props => { 
-                return isAuthenticated ? 
-                <Component {...rest} {...props} /> : 
-                <Redirect to='/login' />
+                return isAuthenticated === true 
+                ? <Component {...rest} {...props} />
+                : <Redirect to='/login' />
             }}
         />
     )
