2352355ba2931016b7dd5aeb37b75272bf928c57
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-03 02:01:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,26 +20,10 @@ const app = express();
 const client = new Discord.Client();
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-client.on('messageReactionAdd', async (reaction, user) => {
-	// When we receive a reaction we check if the reaction is partial or not
-	if (reaction.partial) {
-		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-		try {
-			await reaction.fetch();
-		} catch (error) {
-			console.log('Something went wrong when fetching the message: ', error);
-			// Return as `reaction.message.author` may be undefined/null
-			return;
-		}
-	}
-	// Now the message has been cached and is fully available
-	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-	// The reaction is now also fully available and the properties will be reflected accurately:
-    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-    
-    if(reaction.emoji.name === ":handshake:") {
-        console.log(reaction.users);
-    }
+client.on('ready', async () => {
+    let roleID = '736958886438895657';
+    let membersWithRole = message.guild.roles.get(roleID).members;
+    console.log(`Got ${membersWithRole.size} members with that role.`);
 });
 
 // view engine setup
