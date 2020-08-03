0aa797e8eb59ee12bbed76e9eb4ab15706100b0e
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-03 13:45:51 +0200

    discord login update

--- routes/discord.js
+++ routes/discord.js
@@ -1,11 +1,10 @@
-const discordApi = require('../api/discord');
 const passport = require('passport');
-
 const router = require('express').Router();
 
 router.get('/login', passport.authenticate('discord'));
 router.get('/redirect', passport.authenticate('discord', {
-    failureRedirect: '/forbidden'
+    failureRedirect: '/forbidden',
+    successRedirect: '/profile'
 }), (req, res) => {
     res.send(req.user);
 });
