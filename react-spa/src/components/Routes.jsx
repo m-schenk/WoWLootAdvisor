2bd3808acf14458e67fe38d47e0f19610d8e0356
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-04 15:13:35 +0200

    Update Routes.jsx

--- react-spa/src/components/Routes.jsx
+++ react-spa/src/components/Routes.jsx
@@ -8,6 +8,7 @@ import Profile from './pages/profile/Profile';
 import Members from './pages/council/Members';
 import RaidHistory from './pages/council/RaidHistory';
 import LiveSession from './pages/council/LiveSession';
+import axios from 'axios';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -18,25 +19,39 @@ import './assets/App.css';
 
 
 class Routes extends React.Component {
-    
-    componentDidUpdate() {
-        this.props.overmind.actions.isAuthenticated()
+    constructor() {
+        this.auth = false
     }
 
+    componentDidUpdate() {
+        await axios.get('http://raegae.maarten.ch:3000/api/discord/isauth', { withCredentials: true })
+        .then(response => {
+            console.log(response)
+            if(response.status === 200) {
+                console.log("auth true")
+                this.isAuth = true;
+            } else {
+                this.isAuth = false 
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    
     render() {
-        console.log(this.props.overmind.state.isAuth)
+        console.log(isAuth)
         return(
             <Router>
                     <Switch>
                         <Route path="/login" component={Login} />
                         <div className="justify-content-center align-items-center" id="wrapper">
                             <div className="justify-content-center align-items-center" id="header"><Navbar /></div>
-                                <ProtectedRoute exact path="/" isAuthenticated={this.props.overmind.state.isAuth} component={MainContent} />
-                                <ProtectedRoute path="/wishlist" isAuthenticated={this.props.overmind.state.isAuth} component={MainContent} />
-                                <ProtectedRoute path="/profile" isAuthenticated={this.props.overmind.state.isAuth} component={Profile} />
-                                <ProtectedRoute path="/council/members" isAuthenticated={this.props.overmind.state.isAuth} component={Members} />
-                                <ProtectedRoute path="/council/raidhistory" isAuthenticated={this.props.overmind.state.isAuth} component={RaidHistory} />
-                                <ProtectedRoute path="/council/livesession" isAuthenticated={this.props.overmind.state.isAuth} component={LiveSession} />
+                                <ProtectedRoute exact path="/" isAuthenticated={this.isAuth} component={MainContent} />
+                                <ProtectedRoute path="/wishlist" isAuthenticated={this.isAuth} component={MainContent} />
+                                <ProtectedRoute path="/profile" isAuthenticated={this.isAuth} component={Profile} />
+                                <ProtectedRoute path="/council/members" isAuthenticated={this.isAuth} component={Members} />
+                                <ProtectedRoute path="/council/raidhistory" isAuthenticated={this.isAuth} component={RaidHistory} />
+                                <ProtectedRoute path="/council/livesession" isAuthenticated={this.isAuth} component={LiveSession} />
                             <div className="justify-content-center align-items-center" id="footer"><Footer /></div>
                         </div> 
                     </Switch>
