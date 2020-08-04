2e13c9059d298832ac45e0e6f4c743f6b2c274dd
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-04 18:21:43 +0200

    Update Navbar.jsx

--- react-spa/src/components/Navbar.jsx
+++ react-spa/src/components/Navbar.jsx
@@ -15,11 +15,10 @@ class UserNavbar extends React.Component {
                 <Navbar collapseOnSelect sticky="top" bg="dark" variant="dark" expand="lg">
                     <Image src={require("./assets/logo128x64.png")} />
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Navbar.Brand as={Link} to="/profile">{(this.props.overmind.state.player.name != null) ? this.props.overmind.state.player.name : '$username'}</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/profile">{(this.props.overmind.state.player.name != null) ? this.props.overmind.state.player.name : 'Profile'}</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
                         <Nav.Link as={Link} to="/wishlist">Wishlist</Nav.Link>
                         {(this.props.overmind.state.player.role === 'council') ? 
                         <NavDropdown title="Council" id="basic-nav-dropdown">
