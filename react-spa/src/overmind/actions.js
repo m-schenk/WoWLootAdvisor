b46a72342566a46b75fd9d88d2032920e566960c
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-05 13:41:40 +0200

    Update actions.js

--- react-spa/src/overmind/actions.js
+++ react-spa/src/overmind/actions.js
@@ -36,10 +36,7 @@ export const loadProfile = pipe(
         if (state.player.role === null) complete = false;
         if (state.player.permissions === null) complete = false;
         state.player.isComplete = complete;
-    }),
-    ({ state }) => {
-        return state.player.isComplete
-    }
+    })
 )
 
 export const dragHandler = pipe( //just for now, will become an effect I guess.. later
