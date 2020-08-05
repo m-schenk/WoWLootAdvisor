f13b7c3871641690c3eddc2890f8821fe2a23794
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-06 00:04:01 +0200

    Update Navbar.jsx

--- react-spa/src/components/Navbar.jsx
+++ react-spa/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ import Image from 'react-bootstrap/Image';
 
 class UserNavbar extends React.Component {
     render() {
-        return(
+        return (
             <React.Fragment>
                 <Navbar collapseOnSelect sticky="top" bg="dark" variant="dark" expand="lg">
                     <Image src={require("./assets/logo128x64.png")} />
@@ -19,18 +19,20 @@ class UserNavbar extends React.Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/wishlist">Wishlist</Nav.Link>
-                        {(this.props.overmind.state.player.permissions === 'council') ? 
-                        <NavDropdown title="Council" id="basic-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/council/members">Members</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/council/raidhistory">Raid History</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item as={Link} to="/profile">Raid Live Session</NavDropdown.Item>
-                        </NavDropdown>
-                         : <></>}
+                            <Nav.Link as={Link} to="/wishlist">Wishlist</Nav.Link>
+                            {(this.props.overmind.state.player.permissions === 'council') ?
+                                <NavDropdown title="Council" id="basic-nav-dropdown">
+                                    <NavDropdown.Item as={Link} to="/council/members">Members</NavDropdown.Item>
+                                    <NavDropdown.Item as={Link} to="/council/raidhistory">Raid History</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item as={Link} to="/profile">Raid Live Session</NavDropdown.Item>
+                                </NavDropdown>
+                                : <></>}
                         </Nav>
                     </Navbar.Collapse>
-                    <Nav.Link className="justify-content-end" href="http://raegae.maarten.ch:3000/api/player/logout">Logout</Nav.Link>
+                    <Nav>
+                        <Nav.Link className="justify-content-end" href="http://raegae.maarten.ch:3000/api/player/logout">Logout</Nav.Link>
+                    </Nav>
                 </Navbar>
             </React.Fragment>
         )
