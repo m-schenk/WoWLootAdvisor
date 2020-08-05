407c41309378b09a7f1cfa60d9fb87b7ea99914d
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-06 00:01:51 +0200

    Update Navbar.jsx

--- react-spa/src/components/Navbar.jsx
+++ react-spa/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ class UserNavbar extends React.Component {
                          : <></>}
                         </Nav>
                     </Navbar.Collapse>
-                    <Navbar.Link className="justify-content-end" href="http://raegae.maarten.ch:3000/api/player/logout">Logout</Navbar.Link>
+                    <Nav.Link className="justify-content-end" href="http://raegae.maarten.ch:3000/api/player/logout">Logout</Nav.Link>
                 </Navbar>
             </React.Fragment>
         )
