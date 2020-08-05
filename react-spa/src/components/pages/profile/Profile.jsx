dc55a9cf3dcd750942cec2beb9268dbff986af69
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-05 22:50:23 +0200

    Update Profile.jsx

--- react-spa/src/components/pages/profile/Profile.jsx
+++ react-spa/src/components/pages/profile/Profile.jsx
@@ -24,7 +24,7 @@ class Profile extends React.Component {
         };
     }
 
-    componentDidMount = () => {
+    componentDidMount() {
         this.props.overmind.actions.loadProfile()
             .then(
                 (result) => {
