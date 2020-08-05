f357d0b60bb39bcc202b0066f60014f43467c91e
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-05 04:03:03 +0200

    profile

--- react-spa/src/components/pages/profile/Profile.jsx
+++ react-spa/src/components/pages/profile/Profile.jsx
@@ -15,90 +15,107 @@ import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
 
 class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
     shouldComponentUpdate(nextProps) {
         return this.props.overmind !== nextProps.overmind
     }
 
     componentDidMount() {
-        if (!this.props.overmind.state.player.loaded) {
-            this.props.overmind.actions.getPlayerProfile()
-                .then(
-                    this.props.overmind.actions.isProfileComplete()
-                )
-        }
+        this.props.overmind.actions.loadProfile();
+        // if (!this.props.overmind.state.player.loaded) {
+        //     this.props.overmind.actions.getPlayerProfile()
+        //         .then(
+        //             this.props.overmind.actions.isProfileComplete()
+        //         )
+        // }
     }
 
     render() {
-        return(
-            <Container className="justify-content-center align-items-center" id="main-content">
-                <Row className="row-centered">
-                    <Col className="col-centered" sm={12}>
-                        { !this.props.overmind.state.player.isComplete && <Alert variant={'danger'}>
-                            Your profile seems not complete, please edit your profile before you start creating a wishlist
-                        </Alert>}
-                        <p>{this.props.overmind.state.player._id}</p>
-                        <p>{this.props.overmind.state.player.name}</p>
-                        <p>{this.props.overmind.state.player.class}</p>
-                        <p>{this.props.overmind.state.player.race}</p>
-                        <p>{this.props.overmind.state.player.role}</p>
-                        <p>{this.props.overmind.state.player.aq_attendance}</p>
-                        <p>{this.props.overmind.state.player.naxx_attendance}</p>
-                        <Accordion>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                        Edit Profile
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="0">
-                                    <Card.Body>
-                                        <Form>
-                                            <Form.Group controlId="profile.name">
-                                                <Form.Label>Character name:</Form.Label>
-                                                <Form.Control type="text" />
-                                            </Form.Group>
-                                            <Form.Group controlId="profile.class" onChange={this.classChange}>
-                                                <Form.Label>Select class:</Form.Label>
-                                                <Form.Control as="select">
-                                                    <option>Druid</option>
-                                                    <option>Hunter</option>
-                                                    <option>Mage</option>
-                                                    <option>Paladin</option>
-                                                    <option>Priest</option>
-                                                    <option>Rogue</option>
-                                                    <option>Warlock</option>
-                                                    <option>Warrior</option>
-                                                </Form.Control>
-                                            </Form.Group>
-                                            <Form.Group controlId="profile.class">
-                                                <Form.Label>Select your role:</Form.Label>
-                                                <Form.Control as="select">
-                                                    <option>Dwarf</option>
-                                                    <option>Gnome</option>
-                                                    <option>Human</option>
-                                                    <option>Night Elf</option>
-                                                </Form.Control>
-                                            </Form.Group>
-                                            <Form.Group controlId="profile.class">
-                                                <Form.Label>Select your role:</Form.Label>
-                                                <Form.Control as="select">
-                                                    <option>DPS</option>
-                                                    <option>Heal</option>
-                                                    <option>Tank</option>
-                                                </Form.Control>
-                                            </Form.Group>
-                                            <Button variant="primary" type="submit">
-                                                Submit
-                                            </Button>
-                                        </Form>
-                                    </Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Accordion>
-                    </Col>
-                </Row>
-            </Container>
-        )
+        const { error, isLoaded } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <Container className="justify-content-center align-items-center" id="main-content">
+                    <Row className="row-centered">
+                        <Col className="col-centered" sm={12}>
+                            {!this.props.overmind.state.player.isComplete && <Alert variant={'danger'}>
+                                Your profile seems not complete, please edit your profile before you start creating a wishlist
+                            </Alert>}
+                            <p>{this.props.overmind.state.player._id}</p>
+                            <p>{this.props.overmind.state.player.name}</p>
+                            <p>{this.props.overmind.state.player.class}</p>
+                            <p>{this.props.overmind.state.player.race}</p>
+                            <p>{this.props.overmind.state.player.role}</p>
+                            <p>{this.props.overmind.state.player.aq_attendance}</p>
+                            <p>{this.props.overmind.state.player.naxx_attendance}</p>
+                            <Accordion>
+                                <Card>
+                                    <Card.Header>
+                                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                            Edit Profile
+                                        </Accordion.Toggle>
+                                    </Card.Header>
+                                    <Accordion.Collapse eventKey="0">
+                                        <Card.Body>
+                                            <Form>
+                                                <Form.Group controlId="profile.name">
+                                                    <Form.Label>Character name:</Form.Label>
+                                                    <Form.Control type="text" />
+                                                </Form.Group>
+                                                <Form.Group controlId="profile.class" onChange={this.classChange}>
+                                                    <Form.Label>Select class:</Form.Label>
+                                                    <Form.Control as="select">
+                                                        <option>Druid</option>
+                                                        <option>Hunter</option>
+                                                        <option>Mage</option>
+                                                        <option>Paladin</option>
+                                                        <option>Priest</option>
+                                                        <option>Rogue</option>
+                                                        <option>Warlock</option>
+                                                        <option>Warrior</option>
+                                                    </Form.Control>
+                                                </Form.Group>
+                                                <Form.Group controlId="profile.class">
+                                                    <Form.Label>Select your role:</Form.Label>
+                                                    <Form.Control as="select">
+                                                        <option>Dwarf</option>
+                                                        <option>Gnome</option>
+                                                        <option>Human</option>
+                                                        <option>Night Elf</option>
+                                                    </Form.Control>
+                                                </Form.Group>
+                                                <Form.Group controlId="profile.class">
+                                                    <Form.Label>Select your role:</Form.Label>
+                                                    <Form.Control as="select">
+                                                        <option>DPS</option>
+                                                        <option>Heal</option>
+                                                        <option>Tank</option>
+                                                    </Form.Control>
+                                                </Form.Group>
+                                                <Button variant="primary" type="submit">
+                                                    Submit
+                                                </Button>
+                                            </Form>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                            </Accordion>
+                        </Col>
+                    </Row>
+                </Container>
+            )
+        }
     }
 }
 
