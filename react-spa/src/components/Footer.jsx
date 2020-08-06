2dce88064dc9e9a12d26d143c26ccf3643ed84e4
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-07 00:14:36 +0200

    Update Footer.jsx

--- react-spa/src/components/Footer.jsx
+++ react-spa/src/components/Footer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import NavbarBrand from 'react-bootstrap/NavbarBrand';
-import Container from 'react-bootstrap/Container'
 
 export default class Footer extends React.Component {
     render() {
