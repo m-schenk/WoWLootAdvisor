21efc9dee47eaaa80af198000dfb5b165ea240a7
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-07 00:09:20 +0200

    Update Profile.jsx

--- react-spa/src/components/pages/profile/Profile.jsx
+++ react-spa/src/components/pages/profile/Profile.jsx
@@ -14,6 +14,73 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
 
+const initialFormData = Object.freeze({
+    name: this.props.overmind.state.player.name | "",
+    race: this.props.overmind.state.player.race | "",
+    class: this.props.overmind.state.player.class | "",
+    role: this.props.overmind.state.player.role | ""
+});
+
+const PlayerProfileForm = () => {
+    const [formData, updateFormData] = React.useState(initialFormData);
+
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim()
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.overmind.actions.sendProfile(formData);
+    };
+
+    return (
+        <>
+            <Form onSubmit={(e) => { this.handleSubmit(e) }}>
+                <Form.Group>
+                    <Form.Label>Character name:</Form.Label>
+                    <Form.Control type="text" value={this.state.name} onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Group controlId="profile.class">
+                    <Form.Label>Select class:</Form.Label>
+                    <Form.Control as="select" onChange={this.handleChange}>
+                        <option value="Druid">Druid</option>
+                        <option value="Hunter">Hunter</option>
+                        <option value="Mage">Mage</option>
+                        <option value="Paladin">Paladin</option>
+                        <option value="Priest">Priest</option>
+                        <option value="Rogue">Rogue</option>
+                        <option value="Warlock">Warlock</option>
+                        <option value="Warrior">Warrior</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Select your role:</Form.Label>
+                    <Form.Control as="select" onChange={this.handleChange}>
+                        <option value="Dwarf">Dwarf</option>
+                        <option value="Gnome">Gnome</option>
+                        <option value="Human">Human</option>
+                        <option value="Night Elf">Night Elf</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Select your role:</Form.Label>
+                    <Form.Control as="select" onChange={this.handleChange}>
+                        <option value="DPS">DPS</option>
+                        <option value="Heal">Heal</option>
+                        <option value="Tank">Tank</option>
+                    </Form.Control>
+                </Form.Group>
+                <Button variant="warning" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </>
+    )
+}
+
 class Profile extends React.Component {
     constructor(props) {
         super(props);
@@ -21,16 +88,6 @@ class Profile extends React.Component {
             error: null,
             isLoaded: false,
         };
-    }
-
-    handleChange(event) {
-        //this.setState({ event.target.name: event.target.value });
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.target);
-        this.props.overmind.actions.sendProfile(data);
     }
 
     componentDidMount() {
@@ -69,66 +126,36 @@ class Profile extends React.Component {
                                 <Card.Body>
                                     <Card.Text>
                                         <Row>
-                                            <Col md={6}>
-                                                <label>(remove $dev) _id:</label>
-                                            </Col>
-                                            <Col md={6}>
-                                                <p>{this.props.overmind.state.player._id}</p>
-                                            </Col>
+                                            <Col md={6}><label>(remove $dev) _id:</label></Col>
+                                            <Col md={6}><p>{this.props.overmind.state.player._id}</p></Col>
                                         </Row>
                                         <Row>
-                                            <Col md={6}>
-                                                <label>Character Name:</label>
-                                            </Col>
-
-                                            <Col md={6}>
-                                                <b>{this.props.overmind.state.player.name}</b>
-                                            </Col>
+                                            <Col md={6}><label>Character Name:</label></Col>
+                                            <Col md={6}><b>{this.props.overmind.state.player.name}</b></Col>
                                         </Row>
                                         <Row>
-                                            <Col md={6}>
-                                                <label>Race:</label>
-                                            </Col>
-                                            <Col md={6}>
-                                                <b>{this.props.overmind.state.player.race}</b>
-                                            </Col>
+                                            <Col md={6}><label>Race:</label></Col>
+                                            <Col md={6}><b>{this.props.overmind.state.player.race}</b></Col>
                                         </Row>
                                         <Row>
-                                            <Col md={6}>
-                                                <label>Class:</label>
-                                            </Col>
-                                            <Col md={6}>
-                                                <b>{this.props.overmind.state.player.class}</b>
-                                            </Col>
+                                            <Col md={6}><label>Class:</label></Col>
+                                            <Col md={6}><b>{this.props.overmind.state.player.class}</b></Col>
                                         </Row>
                                         <Row>
-                                            <Col md={6}>
-                                                <label>Role:</label>
-                                            </Col>
-                                            <Col md={6}>
-                                                <b>{this.props.overmind.state.player.role}</b>
-                                            </Col>
+                                            <Col md={6}><label>Role:</label></Col>
+                                            <Col md={6}><b>{this.props.overmind.state.player.role}</b></Col>
                                         </Row>
                                         <Row>
-                                            <Col md={6}>
-                                                <label>aq_attendance:</label>
-                                            </Col>
-                                            <Col md={6}>
-                                                <b>{this.props.overmind.state.player.aq_attendance}</b>
-                                            </Col>
+                                            <Col md={6}><label>aq_attendance:</label></Col>
+                                            <Col md={6}><b>{this.props.overmind.state.player.aq_attendance}</b></Col>
                                         </Row>
                                         <Row>
-                                            <Col md={6}>
-                                                <label>naxx_attendance:</label>
-                                            </Col>
-                                            <Col md={6}>
-                                                <b>{this.props.overmind.state.player.naxx_attendance}</b>
-                                            </Col>
+                                            <Col md={6}><label>naxx_attendance:</label></Col>
+                                            <Col md={6}><b>{this.props.overmind.state.player.naxx_attendance}</b></Col>
                                         </Row>
                                     </Card.Text>
                                 </Card.Body>
                                 <Accordion>
-
                                     <Card.Footer>
                                         <Accordion.Toggle as={Button} variant="link" eventKey="0">
                                             Edit Profile
@@ -136,48 +163,9 @@ class Profile extends React.Component {
                                     </Card.Footer>
                                     <Accordion.Collapse eventKey="0">
                                         <Card.Body>
-                                            <Form onSubmit={(e) => { this.handleSubmit(e) }}>
-                                                <Form.Group>
-                                                    <Form.Label>Character name:</Form.Label>
-                                                    <Form.Control type="text" value={this.state.name} onChange={this.handleChange}/>
-                                                </Form.Group>
-                                                <Form.Group controlId="profile.class">
-                                                    <Form.Label>Select class:</Form.Label>
-                                                    <Form.Control as="select" onChange={this.handleChange}>
-                                                        <option value="Druid">Druid</option>
-                                                        <option value="Hunter">Hunter</option>
-                                                        <option value="Mage">Mage</option>
-                                                        <option value="Paladin">Paladin</option>
-                                                        <option value="Priest">Priest</option>
-                                                        <option value="Rogue">Rogue</option>
-                                                        <option value="Warlock">Warlock</option>
-                                                        <option value="Warrior">Warrior</option>
-                                                    </Form.Control>
-                                                </Form.Group>
-                                                <Form.Group>
-                                                    <Form.Label>Select your role:</Form.Label>
-                                                    <Form.Control as="select" onChange={this.handleChange}>
-                                                        <option value="Dwarf">Dwarf</option>
-                                                        <option value="Gnome">Gnome</option>
-                                                        <option value="Human">Human</option>
-                                                        <option value="Night Elf">Night Elf</option>
-                                                    </Form.Control>
-                                                </Form.Group>
-                                                <Form.Group>
-                                                    <Form.Label>Select your role:</Form.Label>
-                                                    <Form.Control as="select" onChange={this.handleChange}>
-                                                        <option value="DPS">DPS</option>
-                                                        <option value="Heal">Heal</option>
-                                                        <option value="Tank">Tank</option>
-                                                    </Form.Control>
-                                                </Form.Group>
-                                                <Button variant="warning" type="submit">
-                                                    Submit
-                                                    </Button>
-                                            </Form>
+                                            {PlayerProfileForm}
                                         </Card.Body>
                                     </Accordion.Collapse>
-
                                 </Accordion>
                             </Card>
                         </Col>
