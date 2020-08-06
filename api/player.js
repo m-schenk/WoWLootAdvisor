c384f172b854cdfe9c64ecd837455c6d316c75d5
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-06 21:52:57 +0200

    api/player cleanup

--- api/player.js
+++ api/player.js
@@ -1,5 +1,6 @@
 const Player = require('../models/Player');
 const Wishlist = require('../models/Wishlist');
+const createError = require('http-errors');
 const _ = require('lodash');
 
 exports.postPlayerProfile = (req, res, next) => {
@@ -13,60 +14,54 @@ exports.postPlayerProfile = (req, res, next) => {
         aq_attendance: req.body.aq_attendance,
         naxx_attendance: req.body.naxx_attendance,
     });
-
     player.save()
         .then(player => {
             const filteredPlayer = _.omit(player.toObject(), ['discordId'])
-            res.status(201).json({ player: filteredPlayer });
+            res.status(200);
+            res.set({ 'Content-Type': 'text/json' });
+            res.json({ isComplete: true, player: filteredPlayer });
+            res.end();
         })
         .catch(err => {
-            const error = new Error(err);
-            error.message = 'Failed to save new player profile in database (controllers/player saveProfile())';
-            error.httpStatusCode = 500;
-            return next(error);
+            return next(createError(500, 'Failed to save new player profile in database (api/player postPlayerProfile()), error text:' + err));
+        });
+}
+
+exports.getPlayerProfile = (req, res, next) => {
+    Player.findById(req.user._id)
+        .then(player => {
+            const filteredPlayer = _.omit(player.toObject(), ['discordId'])
+            res.status(200);
+            res.set({ 'Content-Type': 'text/json' });
+            res.json({ isComplete: true, player: filteredPlayer });
+            res.end();
+        })
+        .catch(err => {
+            return next(createError(500, 'Failed to fetch player from database (api/player getPlayerProfile()), error text:' + err));
         });
 }
 
 exports.getPlayerById = (req, res, next) => {
     const playerId = req.params.id;
-
     Player.findOne({ id: playerId })
         .populate({ path: 'wishlist', model: Wishlist })
         .then(player => {
-            res.status(200).json({ player: player });
+            const filteredPlayer = _.omit(player.toObject(), ['discordId'])
+            res.status(200);
+            res.set({ 'Content-Type': 'text/json' });
+            res.json({ isComplete: true, player: filteredPlayer });
+            res.end();
         })
         .catch(err => {
-            const error = new Error(err);
-            error.message = 'Failed to fetch player from database (controllers/player getPlayerById())';
-            error.httpStatusCode = 500;
-            return next(error);
+            return next(createError(500, 'Failed to fetch player from database (api/player getPlayerById()), error text:' + err));
         });
 }
 
 exports.logout = (req, res, next) => {
     req.session.destroy((err) => {
-        if(err) {
-            next(err);
+        if (err) {
+            next(createError(500, err));
         }
         res.redirect('http://raegae.maarten.ch:3000/login');
     })
-}
-
-exports.getPlayerProfile = (req, res, next) => {
-    console.log(req.user)
-    Player.findById(req.user._id)
-        .then(player => {
-            const filteredPlayer = _.omit(player.toObject(), ['discordId'])
-            res.status(200);
-            res.set({'Content-Type': 'text/json'});
-            res.json({ isComplete: true, player: filteredPlayer});
-            res.end();
-        })
-        .catch(err => {
-            const error = new Error(err);
-            error.message = 'Failed to fetch player from database (controllers/player getPlayer())';
-            error.httpStatusCode = 500;
-            return next(error);
-        });
-
 }
--- routes/player.js
+++ routes/player.js
@@ -3,7 +3,7 @@ const playerApi = require('../api/player');
 const router = express.Router();
 
 router.post('/postPlayerProfile', playerApi.postPlayerProfile);
-router.get('/getPlayerProfile', playerApi.getPlayerProfile)
+router.get('/getPlayerProfile', playerApi.getPlayerProfile);
 router.get('/logout', playerApi.logout);
 
 router.get('/id::id', playerApi.getPlayerById);
