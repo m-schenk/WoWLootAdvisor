f510399f00ee02e7be414dc4915d1c07a16d4747
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-07 03:04:34 +0200

    styling

--- react-spa/src/components/Navbar.jsx
+++ react-spa/src/components/Navbar.jsx
@@ -7,11 +7,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
 
 class UserNavbar extends React.Component {
     render() {
         return (
-            <>
+            <Container className="container-nav">
                 <Navbar collapseOnSelect sticky="top" bg="dark" variant="dark" expand="lg">
                     <Image src={require("./assets/logo128x64.png")} />
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -34,7 +35,7 @@ class UserNavbar extends React.Component {
                         <Nav.Link className="mr-3" href="http://raegae.maarten.ch:3000/api/player/logout">Logout</Nav.Link>
                     </Nav>
                 </Navbar>
-            </>
+            </Container>
         )
     }
 }
