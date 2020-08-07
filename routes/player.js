d4a242c7bc2fbb27f75eb07b7ec331420b8f09d7
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-08 01:44:07 +0200

    Update player.js

--- routes/player.js
+++ routes/player.js
@@ -8,7 +8,14 @@ const Item = require('../models/Item');
 router.post('/postPlayerProfile', (req, res, next) => {console.log(req.body); next();}, playerApi.validate('postPlayerProfile'), playerApi.postPlayerProfile);
 router.get('/getPlayerProfile', playerApi.getPlayerProfile);
 router.get('/logout', playerApi.logout);
-router.post('/saveWishlist', (req, res, next) => {console.log(req.body); req.body.wishlist.bracket1[0].id = 1; req.body.wishlist.bracket1[1].id = 1; next();},playerApi.validate('postSaveWishlist'), playerApi.postSaveWishlist);
+router.post('/saveWishlist', (req, res, next) => {
+    console.log(req.body); 
+    req.body.wishlist.bracket1[0].itemCategory = 'Reserved';
+    req.body.wishlist.bracket1[1].itemCategory = 'Reserved';
+    req.body.wishlist.bracket1[2].itemCategory = 'Limited';
+    req.body.wishlist.bracket1[3].itemCategory = 'Limited';
+    next();
+},playerApi.validate('postSaveWishlist'), playerApi.postSaveWishlist);
 
 router.get('/id::id', playerApi.getPlayerById);
 
