2c5ee455fca5f8ca7861da38b2533d37661db6b3
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-08 04:01:11 +0200

    Update player.js

--- api/player.js
+++ api/player.js
@@ -115,8 +115,7 @@ exports.postSaveWishlist = (req, res, next) => {
     };
     const err = validationResult(req).formatWith(errorFormatter);
     if (!err.isEmpty()) {
-        return res.status(422).json({errors: err.array()});
-        //return next(createError(422, 'Failed to validate wishlist (api/player postSaveWishlist()), error text: ' + err.array()));
+        return next(createError(422, /*'Failed to validate wishlist (api/player postSaveWishlist()), error text: ' + */err.array()));
     }
     Player.findById(req.user._id)
         .then(player => {
