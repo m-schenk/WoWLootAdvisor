be22c4c9cd844fd58221062bc3b263f0fe1bcacb
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-08 03:34:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -117,7 +117,8 @@ app.use(function (err, req, res, next) {
     } else {
         // render the error page
         res.status(err.statusCode || 500);
-        res.send(err.message)
+        res.json({error: err.message});
+        res.end();
         //res.render('error');
     }
 });
