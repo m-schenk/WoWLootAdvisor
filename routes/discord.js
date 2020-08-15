680261e7ca12f722abe8c3acf4152f1819ed3bea
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-15 23:22:02 +0200

    Update discord.js

--- routes/discord.js
+++ routes/discord.js
@@ -6,7 +6,7 @@ router.get('/redirect', (req, res, next) => {
     console.time('acrt');
     passport.authenticate('discord'), (err, user) => {
         console.timeEnd('acrt');
-        console.log(err)
+        console.log(err);
         if(user) {
             res.redirect(process.env.ADDR + '/profile');
         } else {
