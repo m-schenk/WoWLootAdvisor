83abbb8e78c2c446fdc9b2c05f99e04e68893262
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-15 23:11:20 +0200

    test

--- routes/discord.js
+++ routes/discord.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const router = require('express').Router();
 
 router.get('/login', passport.authenticate('discord'));
-router.get('/redirect', (req, res, next) => {console.timeEnd('api call redirect time'); next(); }, passport.authenticate('discord', {
+router.get('/redirect', (req, res, next) => {console.timeEnd('acrt'); next(); }, passport.authenticate('discord', {
     failureRedirect: process.env.ADDR + '/forbidden',
     successRedirect: process.env.ADDR + '/profile'
 }));
