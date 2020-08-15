272d5e6ed1166b3c151e9c7478e0e69e1746dbe3
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-15 22:35:08 +0200

    Update discord.js

--- routes/discord.js
+++ routes/discord.js
@@ -2,10 +2,10 @@ const passport = require('passport');
 const router = require('express').Router();
 
 router.get('/login', passport.authenticate('discord'));
-router.get('/redirect', () => {console.time('redirecttime'); next();}, passport.authenticate('discord', {
+router.get('/redirect', (req, res, next) => {console.time('redirecttime'); next();}, passport.authenticate('discord', {
     failureRedirect: process.env.ADDR + '/forbidden',
     successRedirect: process.env.ADDR + '/profile'
-}), () => {console.timeEnd('redirecttime'); next();}, (req, res) => {
+}), (req, res, next)  => {console.timeEnd('redirecttime'); next();}, (req, res) => {
     res.send(req.user);
 });
 
