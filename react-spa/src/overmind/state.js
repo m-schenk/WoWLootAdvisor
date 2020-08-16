8d08d935f134e7a7a0593f6ab6c2e03e3bb55448
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-16 03:43:57 +0200

    Update state.js

--- react-spa/src/overmind/state.js
+++ react-spa/src/overmind/state.js
@@ -21,7 +21,7 @@ export const state = {
     wishlist: {
         'locked': false,
         'bracket-1': {
-            itemTypes: new Set(),
+            itemTypes: [],
             'slot-1': {
                 id: '1-1',
                 item: null,
@@ -50,7 +50,7 @@ export const state = {
             'prio-start': 50,
         },
         'bracket-2': {
-            itemTypes: new Set(),
+            itemTypes: [],
             'slot-1': {
                 id: '2-1',
                 item: null,
@@ -79,7 +79,7 @@ export const state = {
             'prio-start': 47,
         },
         'bracket-3': {
-            itemTypes: new Set(),
+            itemTypes: [],
             'slot-1': {
                 id: '3-1',
                 item: null,
@@ -108,7 +108,7 @@ export const state = {
             'prio-start': 44,
         },
         'bracket-4': {
-            itemTypes: new Set(),
+            itemTypes: [],
             'slot-1': {
                 id: '4-1',
                 item: null,
@@ -164,7 +164,7 @@ export const state = {
             'points': Infinity,
             'prio-start': 38,
         },
-        filterList: new Set(),
+        filterList = new Set(),
     },
     liveSearch: {
         'id': 'live-search-id',
