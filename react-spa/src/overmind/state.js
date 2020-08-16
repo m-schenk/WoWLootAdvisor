a25933cac4f6e09ea17606d93a11694755dd4573
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-16 03:57:32 +0200

    Update state.js

--- react-spa/src/overmind/state.js
+++ react-spa/src/overmind/state.js
@@ -164,7 +164,7 @@ export const state = {
             'points': Infinity,
             'prio-start': 38,
         },
-        filterList = new Set(),
+        filterList: new Set()
     },
     liveSearch: {
         'id': 'live-search-id',
