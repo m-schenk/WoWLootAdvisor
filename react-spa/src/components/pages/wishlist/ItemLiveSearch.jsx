61a569d01d444c44844391ae95946954492e8a69
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-17 23:33:03 +0200

    Update ItemLiveSearch.jsx

--- react-spa/src/components/pages/wishlist/ItemLiveSearch.jsx
+++ react-spa/src/components/pages/wishlist/ItemLiveSearch.jsx
@@ -10,6 +10,9 @@ import { tutorialSteps } from '../../tutorialSteps'
 
 import 'shepherd.js/dist/css/shepherd.css'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const tourOptions = {
     defaultStepOptions: {
         cancelIcon: {
