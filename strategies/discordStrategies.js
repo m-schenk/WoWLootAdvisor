6ecb22acde6eff453a3068cab8e6b4d89079eda2
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-18 05:50:34 +0200

    Update discordStrategies.js

--- strategies/discordStrategies.js
+++ strategies/discordStrategies.js
@@ -1,6 +1,7 @@
 const DiscordStrategy = require('passport-discord').Strategy;
 const passport = require('passport');
 const Player = require('../models/Player');
+const Wishlist = require('../models/Wishlist');
 const createError = require('http-errors');
 const _ = require('lodash');
 
@@ -23,14 +24,14 @@ passport.use(new DiscordStrategy({
     // profile is the discord profile, not ours
     if (profile.guilds.filter(entry => (entry.id === process.env.DISCORD_SERVER_ID)).length > 0) {
         Player.findOne({ discordId: profile.id })
-            .populate('wishlist')
+            .populate('Wishlist')
             .then(player => {
                 if (player) {
                     const filteredPlayer = _.omit(player.toObject(), ['discordId']);
                     cb(null, filteredPlayer);
                 } else {
                     console.log('User doesnt exist');
-                    const newPlayer = new Player({ discordId: profile.id, permissions: 'member', wishlist: { locked: false } });
+                    const newPlayer = new Player({ discordId: profile.id, permissions: 'member', wishlist: new Wishlist({ 'locked': false }) });
                     newPlayer.save()
                         .then(player => {
                             const filteredPlayer = _.omit(player.toObject(), ['discordId']);
