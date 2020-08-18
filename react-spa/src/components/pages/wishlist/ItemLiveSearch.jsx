210e6bf705cd4f82f9c906041cabb275b6f2fede
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-18 03:25:18 +0200

    Update ItemLiveSearch.jsx

--- react-spa/src/components/pages/wishlist/ItemLiveSearch.jsx
+++ react-spa/src/components/pages/wishlist/ItemLiveSearch.jsx
@@ -10,7 +10,7 @@ import { tutorialSteps } from '../../tutorialSteps'
 
 import 'shepherd.js/dist/css/shepherd.css'
 
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const tourOptions = {
