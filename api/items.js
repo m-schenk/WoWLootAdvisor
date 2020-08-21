21d52a4d6c9de2328b9e1a492efbd89159116a2c
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-21 18:15:23 +0200

    Update items.js

--- api/items.js
+++ api/items.js
@@ -69,9 +69,7 @@ exports.getQuery = (req, res, next) => {
         .sort({ name: 1 }) //sort items that startswith is stronger than alphabetical //remove class locked items
         .or([{ itemCategory: 'Reserved' }, { itemCategory: 'Limited' }, { itemCategory: 'Unlimited' }]) //dumb way to filter "Unlockable" itemCategory but couldn't find a "not" function
         .then(items => {
-            items.filter(item => {
-                item.classLock.includes("null") || item.classLock.includes(req.user.class);
-            })
+            items.filter(item => (item.classLock.includes("null") || item.classLock.includes(req.user.class)))
             items.sort((a, b) => {
                 return (a.name.search(regex) - b.name.search(regex));
             })
