15467934241fac91af178b019f2a5f29d334520a
Maarten Schenk <maarten.schenk@unibas.ch>
2020-08-24 22:58:37 +0200

    Update MainContent.jsx

--- react-spa/src/components/pages/wishlist/MainContent.jsx
+++ react-spa/src/components/pages/wishlist/MainContent.jsx
@@ -18,47 +18,78 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 class MainContent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+        };
+    }
 
-    onDragEnd = async(result) => {
+    onDragEnd = async (result) => {
         await this.props.overmind.actions.dragHandler(result)
-        .then(event => {
-            if(window.$WowheadPower) {
-                window.$WowheadPower.refreshLinks();
-            }
-            toast(event, {
-                className: 'drag-event-toast',
-                bodyClassName: 'drag-event-toast-textbody',
-                progressClassName: 'drag-event-toast-progress-bar',
-                position: toast.POSITION.TOP_CENTER,
+            .then(event => {
+                if (window.$WowheadPower) {
+                    window.$WowheadPower.refreshLinks();
+                }
+                toast(event, {
+                    className: 'drag-event-toast',
+                    bodyClassName: 'drag-event-toast-textbody',
+                    progressClassName: 'drag-event-toast-progress-bar',
+                    position: toast.POSITION.TOP_CENTER,
+                });
             });
-        });
     };
 
+    componentDidMount() {
+        this.props.overmind.actions.loadProfile()
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
-        if (!this.props.overmind.state.player.isComplete) {
-            return (
-                <Container className="justify-content-center" id="main-content">
-                    <Alert variant={'danger'}>
-                        Your profile is not loaded, please check the profile tab before you start creating a wishlist!
-                    </Alert>
-                </Container>
-            )
+        const { error, isLoaded } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
         } else {
-            return (
-                <Container className="justify-content-center" id="main-content">
-                    <ToastContainer draggable={false} autoClose={5000} />
-                    <DragDropContext onDragEnd={this.onDragEnd}>
-                        <Row className="row-centered">
-                            <Col className="justify-content-center" sm={8}>
-                                <Wishlist />
-                            </Col>
-                            <Col className="justify-content-center" sm={4}>
-                                <ItemLiveSearch id={this.props.overmind.state.liveSearch['id']} />
-                            </Col>
-                        </Row>
-                    </DragDropContext>
-                </Container>
-            )
+            if (!this.props.overmind.state.player.isComplete) {
+                return (
+                    <Container className="justify-content-center" id="main-content">
+                        <Alert variant={'danger'}>
+                            Your profile is not complete, please check the profile tab before you start creating a wishlist!
+                    </Alert>
+                    </Container>
+                )
+            } else {
+                return (
+                    <Container className="justify-content-center" id="main-content">
+                        <ToastContainer draggable={false} autoClose={5000} />
+                        <DragDropContext onDragEnd={this.onDragEnd}>
+                            <Row className="row-centered">
+                                <Col className="justify-content-center" sm={8}>
+                                    <Wishlist />
+                                </Col>
+                                <Col className="justify-content-center" sm={4}>
+                                    <ItemLiveSearch id={this.props.overmind.state.liveSearch['id']} />
+                                </Col>
+                            </Row>
+                        </DragDropContext>
+                    </Container>
+                )
+            }
         }
     }
 }
