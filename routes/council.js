ee287b05464073528fed33c93c441041aff7bd2d
Maarten Schenk <maarten.schenk@unibas.ch>
2020-09-03 14:51:24 +0200

    Update council.js

--- routes/council.js
+++ routes/council.js
@@ -5,5 +5,6 @@ const router = express.Router();
 /* GET users listing. */
 router.post('/lockMember', councilApi.lockMember);
 router.post('/unlockMember', councilApi.unlockMember);
+router.get('/player/:name', councilApi.validate('player'), councilApi.player);
 
 module.exports = router;
